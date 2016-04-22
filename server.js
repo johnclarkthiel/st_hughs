86a92131d3150342a8d83ad8a3f7765b436f2ffb
John <johnclarkthiel@Johns-MBP.home>
2016-04-22 12:28:40 -0400

    updating

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var port = process.env.PORT || 3000;
 // var morgan = require('morgan');
 
 
-var mongoose = require('mongoose');
-var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost/st_hughs';
+// var mongoose = require('mongoose');
+// var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost/st_hughs';
 
 
 
@@ -26,21 +26,21 @@ app.get('/contact', function(req,res){
 	res.redirect('/');
 });
 
-var staticController = require('./controllers/staticController.js');
-app.use('/static', staticController);
+// var staticController = require('./controllers/staticController.js');
+// app.use('/static', staticController);
 
 
 //mongoose
-mongoose.connect(mongoURI);
+// mongoose.connect(mongoURI);
 
-mongoose.connection.on('error', function(){
-	console.log('MONGO not connected');
-});
+// mongoose.connection.on('error', function(){
+// 	console.log('MONGO not connected');
+// });
 
 //port
-mongoose.connection.once('open', function(){
-		console.log('MONGO connected');
+// mongoose.connection.once('open', function(){
+// 		console.log('MONGO connected');
 		app.listen(port, function(){
 		console.log("Listening on port:" + port);
 	});
-});
+// });
