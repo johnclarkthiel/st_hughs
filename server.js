ab58d6f73ae6b9abb806cc45218bd567aa45332d
John <johnclarkthiel@Johns-MBP.home>
2016-04-22 11:30:40 -0400

    doing stuff

--- server.js
+++ server.js
@@ -1,24 +1,40 @@
 //Requirements
 var express = require('express');
 var app = express();
-var morgan = require("morgan");
 
 var port = process.env.PORT || 3000;
+
+var morgan = require('morgan');
+
 
 var mongoose = require('mongoose');
 var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost/st_hughs';
 
 
-//middleware and require public folder
+
+//middleware
 app.use(express.static('public'));
 app.use(morgan('dev'));
+
+
+
+app.get('/', function(req,res){
+	res.send('HELLO');
+});
+
+app.get('/contact', function(req,res){
+	res.redirect('/');
+});
+
+var staticController = require('./controllers/staticController.js');
+app.use('/static', staticController);
 
 
 //mongoose
 mongoose.connect(mongoURI);
 
 mongoose.connection.on('error', function(){
-	console.log('MONGO not connected for some reason');
+	console.log('MONGO not connected');
 });
 
 //port
