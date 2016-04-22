a95b13d9382910f417afdea7890c73a89c3a82b0
John <johnclarkthiel@Johns-MBP.home>
2016-04-22 12:36:21 -0400

    updating styling

--- server.js
+++ server.js
@@ -4,19 +4,8 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-// var morgan = require('morgan');
-
-
-// var mongoose = require('mongoose');
-// var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost/st_hughs';
-
-
-
 //middleware
 app.use(express.static('public'));
-// app.use(morgan('dev'));
-
-
 
 app.get('/', function(req,res){
 	res.send('HELLO');
@@ -26,21 +15,6 @@ app.get('/contact', function(req,res){
 	res.redirect('/');
 });
 
-// var staticController = require('./controllers/staticController.js');
-// app.use('/static', staticController);
-
-
-//mongoose
-// mongoose.connect(mongoURI);
-
-// mongoose.connection.on('error', function(){
-// 	console.log('MONGO not connected');
-// });
-
-//port
-// mongoose.connection.once('open', function(){
-// 		console.log('MONGO connected');
-		app.listen(port, function(){
-		console.log("Listening on port:" + port);
-	});
-// });
+app.listen(port, function(){
+	console.log("Listening on port:" + port);
+});
